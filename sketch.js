4805ce54f865b22a416e5362a6b82abf5c91901c
kazuakixz <161736766+kazuakixz@users.noreply.github.com>
2024-10-03 09:02:14 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+let campoIdade;
+let campoFantasia;
+let campoAventura;
+
+function setup() {
+  createCanvas(800, 400);
+  createElement("h2", "Recomendador de filmes");
+  createSpan("Sua idade:");
+  campoIdade = createInput("5");
+  campoFantasia = createCheckbox("gosta de Romance Anime?");
+  campoAventura = createCheckbox("Gosta de Drama?");
+}
+
+function draw() {
+  background("white");
+  let idade = campoIdade.value();
+  let gostaDeRomanceAnime = campoFantasia.checked();
+  let gostaDeAventura = campoAventura.checked();
+  let recomendacao = geraRecomendacao(idade, gostaDeRomanceAnime, gostaDeAventura);
+
+  fill(color(76, 0, 115));
+  textAlign(CENTER, CENTER);
+  textSize(38);
+  text(recomendacao, width / 2, height / 2);
+}
+
+function geraRecomendacao(idade, gostaDeRomanceAnime, ) {
+  if (idade >= 10) {
+    if (idade >= 14) {
+      return "Violet Evergarden";
+    } else {
+      if (idade >= 15) {
+        if(gostaDeRomanceAnime || gostaDeDrama) {
+          return "Eu Quero Comer seu Pâncreas";          
+        } else{
+         return "Uma Linda Mulher";
+        }
+      } else {
+        if (gostaDeRomanceAnime) {
+          return "Olhos de Gato";
+        } else {
+          return "O tempo com você";
+        }
+      }
+    }
+  } else {
+    if (gostaDeRomanceAnime) {
+      return "Kimi no na wa";
+    } else {
+      return "Como Eu Era Antes de Você";
+    }
+  }
+}
